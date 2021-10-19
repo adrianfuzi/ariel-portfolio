9998e6df69884f2b94b52f01edbfed03fdea46d0
adrianfuzi <adew05@gmail.com>
2021-10-19 12:00:54 +0200

    Fix build error

--- src/components/projects-other.js
+++ src/components/projects-other.js
@@ -3,90 +3,53 @@ import { StaticImage } from "gatsby-plugin-image"
 import { Link } from "gatsby"
 
 const ProjectsOther = () => {
-  if (typeof window !== "undefined") {
-    const currentPath = window.location.pathname
-    const p1 = "/handyhelper"
-    const p2 = "/perfectproperties"
+  const currentPath = window.location.pathname
+  const p1 = "/handyhelper"
+  const p2 = "/perfectproperties"
 
-    if (currentPath === p1) {
-      return (
-        <>
-          <div className="other">
-            <h3>See Other Projects</h3>
-            <div className="other__container">
-              <div className="other__container--card">
-                <Link to="/perfectproperties">
-                  <StaticImage
-                    className="other__container--card-img"
-                    src="../images/project-2/cover.png"
-                    width={300}
-                    quality={95}
-                    formats={["AUTO", "WEBP", "AVIF"]}
-                    alt="Perfect Properties"
-                  />
-                  <p>Perfect Properties</p>
-                </Link>
-              </div>
-
-              <div className="other__container--card">
-                <Link>
-                  <StaticImage
-                    className="other__container--card-img"
-                    src="../images/project-3/cover.png"
-                    width={300}
-                    quality={95}
-                    formats={["AUTO", "WEBP", "AVIF"]}
-                    alt="WordPal"
-                  />
-                  <p>WordPal</p>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </>
-      )
-    } else if (currentPath === p2) {
-      return (
-        <>
-          <div className="other">
-            <h3>See Other Projects</h3>
-            <div className="other__container">
-              <div className="other__container--card">
-                <Link to="/handyhelper">
-                  <StaticImage
-                    className="other__container--card-img"
-                    src="../images/project-1/cover.png"
-                    width={300}
-                    quality={95}
-                    formats={["AUTO", "WEBP", "AVIF"]}
-                    alt="HandyHelper"
-                  />
-                  <p>HandyHelper</p>
-                </Link>
-              </div>
-
-              <div className="other__container--card">
-                <Link>
-                  <StaticImage
-                    className="other__container--card-img"
-                    src="../images/project-3/cover.png"
-                    width={300}
-                    quality={95}
-                    formats={["AUTO", "WEBP", "AVIF"]}
-                    alt="WordPal"
-                  />
-                  <p>WordPal</p>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </>
-      )
-    } else {
-      return (
-        <>
+  if (currentPath === p1) {
+    return (
+      <>
+        <div className="other">
           <h3>See Other Projects</h3>
-          <div className="other">
+          <div className="other__container">
+            <div className="other__container--card">
+              <Link to="/perfectproperties">
+                <StaticImage
+                  className="other__container--card-img"
+                  src="../images/project-2/cover.png"
+                  width={300}
+                  quality={95}
+                  formats={["AUTO", "WEBP", "AVIF"]}
+                  alt="Perfect Properties"
+                />
+                <p>Perfect Properties</p>
+              </Link>
+            </div>
+
+            <div className="other__container--card">
+              <Link>
+                <StaticImage
+                  className="other__container--card-img"
+                  src="../images/project-3/cover.png"
+                  width={300}
+                  quality={95}
+                  formats={["AUTO", "WEBP", "AVIF"]}
+                  alt="WordPal"
+                />
+                <p>WordPal</p>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </>
+    )
+  } else if (currentPath === p2) {
+    return (
+      <>
+        <div className="other">
+          <h3>See Other Projects</h3>
+          <div className="other__container">
             <div className="other__container--card">
               <Link to="/handyhelper">
                 <StaticImage
@@ -102,22 +65,57 @@ const ProjectsOther = () => {
             </div>
 
             <div className="other__container--card">
-              <Link to="/perfectproperties">
+              <Link>
                 <StaticImage
                   className="other__container--card-img"
-                  src="../images/project-2/cover.png"
+                  src="../images/project-3/cover.png"
                   width={300}
                   quality={95}
                   formats={["AUTO", "WEBP", "AVIF"]}
-                  alt="Perfect Properties"
+                  alt="WordPal"
                 />
-                <p>Perfect Properties</p>
+                <p>WordPal</p>
               </Link>
             </div>
           </div>
-        </>
-      )
-    }
+        </div>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <h3>See Other Projects</h3>
+        <div className="other">
+          <div className="other__container--card">
+            <Link to="/handyhelper">
+              <StaticImage
+                className="other__container--card-img"
+                src="../images/project-1/cover.png"
+                width={300}
+                quality={95}
+                formats={["AUTO", "WEBP", "AVIF"]}
+                alt="HandyHelper"
+              />
+              <p>HandyHelper</p>
+            </Link>
+          </div>
+
+          <div className="other__container--card">
+            <Link to="/perfectproperties">
+              <StaticImage
+                className="other__container--card-img"
+                src="../images/project-2/cover.png"
+                width={300}
+                quality={95}
+                formats={["AUTO", "WEBP", "AVIF"]}
+                alt="Perfect Properties"
+              />
+              <p>Perfect Properties</p>
+            </Link>
+          </div>
+        </div>
+      </>
+    )
   }
 }
 
